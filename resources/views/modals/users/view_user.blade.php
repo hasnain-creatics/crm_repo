5b206e4fc3e5ccc58fe9af96de6f6e536ce674ed
crm-creatics <creatics.developer2@gmail.com>
2022-05-10 09:56:25 +0500

    5 10 2022 9 56

---
+++ resources/views/modals/users/view_user.blade.php
@@ -0,0 +1,15 @@
+        <div class="modal fade effect-slide-in-right " id="modaldemo8" style="display: block; padding-right: 17px;" aria-modal="true" role="dialog">
+			<div class="modal-dialog modal-lg modal-dialog-centered text-center" role="document">
+				<div class="modal-content modal-content-demo">
+					<div class="modal-header">
+						<h6 class="modal-title">User Details</h6><button aria-label="Close" class="btn-close" data-bs-dismiss="modal" type="button"><span aria-hidden="true">×</span></button>
+					</div>
+					<div class="modal-body">
+						<?php print_r($users_details);?>
+					</div>
+					<div class="modal-footer">
+						<button class="btn btn-primary" type="button">Save changes</button> <button class="btn btn-secondary" data-bs-dismiss="modal" type="button">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
